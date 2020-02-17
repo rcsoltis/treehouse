32c58a5f3df94f0fe500054b7443847d2b30cc48
Cary Soltis <rcsoltis@yahoo.com>
2020-02-16 18:21:33 -0800

    Add while loop example

--- inc/loops.php
+++ inc/loops.php
@@ -1,12 +1,20 @@
 <?php
 
-//IN PROGRESS
+//prints 100 years, with the current year as the final year
+//No newline in order to save space
 
-   /* $currentYear = date('Y');
-    $year = $currentYear - 100;
+$currentYear = date('Y');
+    $year = $currentYear - 99;
 
-    while (++$year <= $currentYear) {
-        echo $year . "</ br>\n";
-    } */
+while ($year <= $currentYear) {
+    echo $year . " </ br>";
+    $year++;
+}
+
+//alternative way to display 101 years
+
+/*do {
+    echo $year . " </ br>";
+} while (++$year <= $currentYear);*/
 
 ?>
