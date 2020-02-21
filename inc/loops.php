54462468b8951a95ae816b7ea15e94ad6be1044d
Cary Soltis <rcsoltis@yahoo.com>
2020-02-21 14:45:05 -0800

    add for loop

--- inc/loops.php
+++ inc/loops.php
@@ -3,18 +3,24 @@
 //prints 100 years, with the current year as the final year
 //No newline in order to save space
 
-$currentYear = date('Y');
-    $year = $currentYear - 99;
+date_default_timezone_set('America/Los_Angeles');//change zone as per need
 
-while ($year <= $currentYear) {
+$currentYear = date('Y');
+
+for ($year = $currentYear - 99; $year <= $currentYear; $year++ ) {
+    echo $year . " </ br>";
+}
+
+/*while ($year <= $currentYear) {
     echo $year . " </ br>";
     $year++;
-}
+}*/
 
 //alternative way to display 101 years
 
 /*do {
     echo $year . " </ br>";
 } while (++$year <= $currentYear);*/
+
 
 ?>
