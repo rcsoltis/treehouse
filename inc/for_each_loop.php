5e80014a986ac96e0a9fe6490f1941992856c6a5
Cary Soltis <rcsoltis@yahoo.com>
2020-02-21 14:54:41 -0800

    add for loop

--- inc/for_each_loop.php
+++ inc/for_each_loop.php
@@ -1,11 +1,15 @@
 <?php
 
-$learn = array('PHP','Phython','algorithms','APIs','Linux server');
+$learn = array('PHP','Python','algorithms','APIs','Linux server');
 asort($learn);
 
-$count = 0;
+/*$count = 0;
 foreach ($learn as $v) {
     echo "$v <br>\n";
+}*/
+
+for ($i = 0; $i < count($learn); $i++) {
+    echo $learn[$i] . "\n";
 }
 
 ?>
