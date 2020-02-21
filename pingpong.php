039326bd1fcc9bdb92f62bc8219d96a30aad9629
Cary Soltis <rcsoltis@yahoo.com>
2020-02-21 14:08:49 -0800

    add ping pong game

---
+++ pingpong.php
@@ -0,0 +1,33 @@
+<?php
+$player1 = 0;
+$player2 = 0;
+$round = 0;
+
+//WIN
+//player must reach a score of 11
+//player must be a minimum of 2 points higher than opponent
+
+while (abs($player1 - $player2) < 2 || ($player1 < 11 && $player2 < 11)) {
+//NOT WIN
+//randomly increment one of the player scores each round
+    $round++;
+    echo "<h2>Round $round</h2>\n";
+    if (rand(0,1)) {
+        $player1++;
+    } else {
+        $player2++;
+    }
+    echo "Player1 = $player1<br />\n";
+    echo "Player2 = $player2<br />\n";
+}
+
+echo "<h1>";
+if ($player1 > $player2) {
+    echo "Player1";
+} else {
+    echo "Player2";
+}
+
+echo " is the winner after $round rounds!</h1>\n";
+
+?>
