0855b758bd62c19f4415be79fc57201d4fdfe5ec
Cary Soltis <rcsoltis@yahoo.com>
2020-03-02 18:18:04 -0800

    sort array

--- todo.php
+++ todo.php
@@ -2,12 +2,18 @@
 include 'inc/list.php';
 
 $status = false;
+$field = 'priority';
 $filter = array();
-foreach ($list as $key => $item) {
-    if ($item['complete'] == $status) {
-        $filter[] = $key;
+foreach ($list as $originalKey => $item) {
+    if ($status == 'all' || $item['complete'] == $status) {
+        if (isset($field) && isset($item[$field])) {
+            $filter[$originalKey] = $item[$field];
+        } else {
+            $filter[$originalKey] = $item['priority'] + 12;
+        }
     }
 }
+asort($filter);
 //echo '<pre>';
 //var_dump($filter, $list);
 //echo '</pre>';
@@ -21,7 +27,7 @@ echo '<th>Due Date</th>';
 echo '<th>Complete</th>';
 echo '</tr>';
 
-foreach ($filter as $id) {
+foreach ($filter as $id => $value) {
     echo '<tr>';
     echo '<td>' . $list[$id]['title'] . "</td>\n";
     echo '<td>' . $list[$id]['priority'] . "</td>\n";
